3d835f7867c41f9605b8433d0514601139c46881
Matyi Bálint <matybal.m@gmail.com>
2021-10-27 15:33:34 +0200

    first route added

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+app.get("/hello", (req, res) => {
+  res.send("Task Manager");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, console.log(`app is listening on port ${PORT}`));
