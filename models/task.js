7587390ea2a14a185b52fb18694736f834ac0521
Matyi Bálint <matybal.m@gmail.com>
2021-10-27 23:46:47 +0200

    feat(model):basic validation

--- models/task.js
+++ models/task.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  complated: Boolean,
+  name: {
+    type: String,
+    required: [true, "You must provide a name!"],
+    trim: true,
+    maxlength: [20, "Task name must be under 20 char!"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
