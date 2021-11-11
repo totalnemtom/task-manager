e75ffcf7bd2b2c0dbbc8575f28a524779581f1d6
Matyi Bálint <62192787+totalnemtom@users.noreply.github.com>
2021-11-11 14:05:04 +0100

    CAL-9 (#3)
    * feat(error handling): added next functions to controller
    
    * feat(error handling): updated error handling

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,6 +1,7 @@
 const Task = require("../models/task");
 const asyncWrapper = require("../middleware/async");
 const { createCustomError } = require("../errors/custom-errors");
+const Logger = require("../winston/logger");
 
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find();
@@ -12,23 +13,39 @@ const createTask = asyncWrapper(async (req, res) => {
   res.status(201).json({ task });
 });
 
-const getTask = asyncWrapper(async (req, res) => {
+const getTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findOne({ _id: taskID });
   if (!task) {
-    return next(createCustomError(`No task with id: ${taskID}`, 404));
+    return next(
+      createCustomError(
+        "not found error",
+        404,
+        true,
+        `No task with id: ${taskID}`
+      ),
+      Logger.log("error", "Task not found")
+    );
   }
   res.status(200).json({ task });
 });
 
-const updateTask = asyncWrapper(async (req, res) => {
+const updateTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
     new: true,
     runValidators: true,
   });
   if (!task) {
-    return next(createCustomError(`No task with id: ${taskID}`, 404));
+    return next(
+      createCustomError(
+        "not found error",
+        404,
+        true,
+        `No task with id: ${taskID}`
+      ),
+      Logger.log("error", "Couldn't update task")
+    );
   }
   res.status(200).json({ task });
 });
@@ -37,7 +54,15 @@ const deleteTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndDelete({ _id: taskID });
   if (!task) {
-    return next(createCustomError(`No task with id: ${taskID}`, 404));
+    return next(
+      createCustomError(
+        "not found error",
+        404,
+        true,
+        `No task with id: ${taskID}`
+      ),
+      Logger.log("error", "Couldn't delete task")
+    );
   }
   res.status(200).json({ task });
 });
