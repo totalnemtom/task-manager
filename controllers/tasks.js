14e0e0949587873b92d11f9fbfe84ba7083f922f
Matyi Bálint <matybal.m@gmail.com>
2021-11-11 14:11:03 +0100

    feat(error handling): added httpStatusCodes enum

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,6 +1,12 @@
 const Task = require("../models/task");
 const asyncWrapper = require("../middleware/async");
+<<<<<<< Updated upstream
 const { createCustomError } = require("../errors/custom-errors");
+=======
+const Logger = require("../winston/logger");
+const { createCustomError } = require("../errors/custom-errors");
+const httpStatusCodes = require("../errors/status-codes");
+>>>>>>> Stashed changes
 
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find();
@@ -16,7 +22,19 @@ const getTask = asyncWrapper(async (req, res) => {
   const { id: taskID } = req.params;
   const task = await Task.findOne({ _id: taskID });
   if (!task) {
+<<<<<<< Updated upstream
     return next(createCustomError(`No task with id: ${taskID}`, 404));
+=======
+    return next(
+      createCustomError(
+        "not found error",
+        httpStatusCodes.NOT_FOUND,
+        true,
+        `No task with id: ${taskID}`
+      ),
+      Logger.log("error", "Task not found")
+    );
+>>>>>>> Stashed changes
   }
   res.status(200).json({ task });
 });
@@ -28,7 +46,19 @@ const updateTask = asyncWrapper(async (req, res) => {
     runValidators: true,
   });
   if (!task) {
+<<<<<<< Updated upstream
     return next(createCustomError(`No task with id: ${taskID}`, 404));
+=======
+    return next(
+      createCustomError(
+        "not found error",
+        httpStatusCodes.NOT_FOUND,
+        true,
+        `No task with id: ${taskID}`
+      ),
+      Logger.log("error", "Couldn't update task")
+    );
+>>>>>>> Stashed changes
   }
   res.status(200).json({ task });
 });
@@ -37,7 +67,19 @@ const deleteTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndDelete({ _id: taskID });
   if (!task) {
+<<<<<<< Updated upstream
     return next(createCustomError(`No task with id: ${taskID}`, 404));
+=======
+    return next(
+      createCustomError(
+        "not found error",
+        httpStatusCodes.NOT_FOUND,
+        true,
+        `No task with id: ${taskID}`
+      ),
+      Logger.log("error", "Couldn't delete task")
+    );
+>>>>>>> Stashed changes
   }
   res.status(200).json({ task });
 });
