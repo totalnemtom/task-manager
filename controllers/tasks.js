0d8dfcd139b8b3d804662f0b01bc7016b43a7730
Matyi Bálint <matybal.m@gmail.com>
2021-11-22 11:28:58 +0100

    fix(winston)

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -4,17 +4,17 @@ const Logger = require("../winston/logger");
 const { createCustomError } = require("../errors/custom-errors");
 const httpStatusCodes = require("../errors/status-codes");
 
-const getAllTasks = asyncWrapper(async (req, res) => {
+const getAllTasks = asyncWrapper(async (req, res, next) => {
   const tasks = await Task.find();
   res.status(200).json({ tasks });
 });
 
-const createTask = asyncWrapper(async (req, res) => {
+const createTask = asyncWrapper(async (req, res, next) => {
   const task = await Task.create(req.body);
   res.status(201).json({ task });
 });
 
-const getTask = asyncWrapper(async (req, res) => {
+const getTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findOne({ _id: taskID });
   if (!task) {
@@ -31,7 +31,7 @@ const getTask = asyncWrapper(async (req, res) => {
   res.status(200).json({ task });
 });
 
-const updateTask = asyncWrapper(async (req, res) => {
+const updateTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
     new: true,
