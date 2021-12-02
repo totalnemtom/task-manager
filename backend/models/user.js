89c6b6604d8df3123646db3bd132d9f928055379
Matyi Bálint <matybal.m@gmail.com>
2021-12-02 11:12:04 +0100

    refactor

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,9 +1,5 @@
 const mongoose = require("mongoose");
-
-var validateEmail = function (email) {
-  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  return re.test(email);
-};
+const validateEmail = require("../lib/user/validation");
 
 const userSchema = new mongoose.Schema({
   firstName: {
